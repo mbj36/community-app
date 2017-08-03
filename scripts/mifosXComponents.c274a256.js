3f7ce41f20e6432ea812249242d5492697476635
mbj36 <mohitbajo36@gmail.com>
2017-08-03 20:22:50 +0530

    Updates

---
+++ scripts/mifosXComponents.c274a256.js
@@ -0,0 +1,39 @@
+define(['Q', 'underscore', 'mifosX'], function (Q) {
+    var components = {
+        models: [
+            'models.f74aacf1'
+        ],
+        services: [
+            'ResourceFactoryProvider',
+            'HttpServiceProvider',
+            'AuthenticationService',
+            'SessionManager',
+            'Paginator',
+            'UIConfigService',
+            'NotificationResponseHeaderProvider'
+        ],
+        controllers: [
+            'controllers.94c77252'
+        ],
+        filters: [
+            'filters.34474998'
+        ],
+        directives: [
+            'directives.a824a551'
+        ]
+    };
+
+    return function() {
+        var defer = Q.defer();
+        require(_.reduce(_.keys(components), function (list, group) {
+            return list.concat(_.map(components[group], function (name) {
+                return group + "/" + name;
+            }));
+        }, [
+            'routes-initialTasks-webstorage-configuration.0d972d51'
+        ]), function(){
+            defer.resolve();
+        });
+        return defer.promise;
+    }
+});
